d38f9c6426e4453f8f7508a83f8280005abe0a96
Pavel <ilmarinnen@list.ru>
2022-05-26 11:54:00 +0300

    Project 0 optimal added

---
+++ project_0_optimal/README.md
@@ -0,0 +1,69 @@
+# Project 0 Optimal. Guess the Number
+
+## List of contents
+
+[1. Project description](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#Project-description)
+
+[2. What case we are solwing?](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#What-case-we-are-solwing)
+
+[3. Shorts about data](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#Shorts-about-data)
+
+[4. Project stages](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#Project-stages)
+
+[5. Results](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#Results)
+
+[6. Conclusions](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#Conclusions)
+
+
+### Project description
+Guess the number, randomly given by PC, in minimum amount of tryes
+
+:arrow_up:[to list of contents](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#List-of-contents)
+
+### What case we are solwing?
+We need to dewelop a programm, which could guess randomly given number in minimum amount of tryes
+
+**Terms:**
+- PC randomly gives us a number between 1 and 100, and we need to guess it. "Guess" means we need to dewelop a programm that could guess the number
+- Algorithm should consider if number in current iteration is more or less then one we need to guess
+
+**Quality metrics**
+
+Results evaluates on mean amount of tryes within 1000 cicles. Amount of tryes schould be as small as possible.
+
+**Practice goal**
+
+Nice and clean code on Python writing practice
+IDE working practice
+GitHub working practice
+
+:arrow_up:[to list of contents](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#List-of-contents)
+
+### Shorts about data
+
+Randomly given number between 1 and 100
+
+:arrow_up:[to list of contents](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#List-of-contents)
+
+### Project stages
+
+Only one for now.
+
+:arrow_up:[to list of contents](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#List-of-contents)
+
+### Results
+
+Three functions were created:
+- Simplest one just randomly guessing the number.
+- Adwanced one still randomly guessing the number - but within schrinking borders
+- Final one utilise idea of shrinking borders without any random at all.
+
+:arrow_up:[to list of contents](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#List-of-contents)
+
+### Conclusions
+
+Not so hard, but prety usefull if we talk about expirience.
+Also we can make an obvious conclusion - it`s always better when you know what are you doing.
+According to our game - than less we randomize than better is outcome.
+
+:arrow_up:[to list of contents](https://github.com/Nokachishikime/sf_data_science/tree/main/project_0/README.md#List-of-contents)
---
+++ project_0_optimal/game_optimal.py
@@ -0,0 +1,108 @@
+"""Guess number game - optimal version
+The goal is to minimize amount of tryes
+"""
+
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    """Randomly guessing the number
+
+    Args:
+        number (int, optional): Given number. Defaults to 1.
+
+    Returns:
+        int: Amount of tryes
+    """
+    
+    count = 0
+    
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)
+        if number == predict_number:
+            break
+    return count
+
+def wise_predict(number:int=1) -> int:
+    """Wisely guessing the number: number seed is stil random, but borders of seed are shrinking
+
+    Args:
+        number (int, optional): Given number. Defaults to 1.
+
+    Returns:
+        int: Amount of tryes
+    """
+    
+    count = 0
+    min_number = 1 #basic minimum border
+    max_number = 100 #basic maximum border
+    
+    while True:
+        count += 1
+        predict_number = np.random.randint(min_number, max_number+1)
+        if number > predict_number:
+            min_number = predict_number
+        elif number < predict_number:
+            max_number = predict_number
+        else:
+            break
+    return count
+
+def exect_predict(number:int=1) -> int:
+    """Logicaly guessing the number: shrinking borders catching the number sooner or later
+
+    Args:
+        number (int, optional): Given number. Defaults to 1.
+
+    Returns:
+        int: Amount of tryes
+    """
+    
+    count = 0
+    min_number = 1 #basic minimum border
+    max_number = 101 #basic maximum border
+    
+    while True:
+        count += 1
+        predict_number = (min_number + max_number) // 2
+        if number > predict_number:
+            min_number = predict_number
+        elif number < predict_number:
+            max_number = predict_number
+        else:
+            break
+    return count
+
+def score_game(some_predict) -> int:
+    """Mean amount of tryes to guess number within 1000 guessing cycles
+
+    Args:
+        some_predict (_type_): Some guessing function
+
+    Returns:
+        int: mean amount of tryes
+    """
+    count_list = [] #list to save tryes amount
+    np.random.seed(1) #make the seed stable for reproducibility
+    random_array = np.random.randint(1, 101, size=(1000)) #list of numbers
+    
+    for number in random_array:
+        count_list.append(some_predict(number))
+
+    score = int(np.mean(count_list)) #mean amount of tryes calculation
+    print(f"With function {some_predict.__name__} mean amount of tryes is: {score}")
+    return score
+
+#print(f"Number of tryes: {wise_predict()}")
+
+#some_number = np.random.randint(1, 101)
+#print(f"Number of tryes: {exect_predict(some_number)} for number {some_number}")
+
+#score_game(random_predict)
+#score_game(wise_predict)
+#score_game(exect_predict)
+
+if __name__ == '__main__':
+    score_game(random_predict)
+    score_game(wise_predict)
+    score_game(exect_predict)
